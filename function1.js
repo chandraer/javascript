debe9c9c793bf3e0d89bb672662c27e57d8101c9
sairamprasadbandi <180030591@kluniversity.in>
2025-05-04 18:49:36 +0530

    Iam second time commiting your code

--- function1.js
+++ function1.js
@@ -1,10 +1,6 @@
 let addition=(a,b)=> {
-<<<<<<< HEAD
-let summation=a+b;
-return summation;
-=======
-let suming=a+b;
+let summing=a+b;
 return summing;
->>>>>>> a2b29d844da39b9ee2e28ff82b2eeae702154456
+
 }
 console.log(addition(10,25));
